65238817f30bdcf234e6f4491be60b3ea377e26e
monroestephenson <140461945+monroestephenson@users.noreply.github.com>
2026-03-08 15:17:23 +0100

    Update opn to v0.2.1

--- Formula/opn.rb
+++ Formula/opn.rb
@@ -1,8 +1,8 @@
 class Opn < Formula
   desc "Modern, human-friendly replacement for lsof"
   homepage "https://github.com/monroestephenson/opn"
-  url "https://github.com/monroestephenson/opn/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0451095a6d9e31ca2bbbb612503475349a08a9192fe3fedb96a6555ef4d2aee4"
+  url "https://github.com/monroestephenson/opn/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "55283bcd02f89dfffbeeb25bb3ae96f70197b9b60042b1b646d07943f3dec635"
   license "MIT"
 
   depends_on "rust" => :build

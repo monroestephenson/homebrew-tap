a0b3530d5e716756c9c25f2df93102590c2e4224
Monroe Stephenson <m@gramscian.com>
2026-03-12 10:20:13 +0100

    Update opn to v0.4.0

--- Formula/opn.rb
+++ Formula/opn.rb
@@ -1,9 +1,8 @@
 class Opn < Formula
   desc "Modern, human-friendly replacement for lsof"
   homepage "https://github.com/monroestephenson/opn"
-  url "https://github.com/monroestephenson/opn/archive/refs/tags/v0.3.1.tar.gz"
-  version "0.3.1"
-  sha256 "ccd7a2eed3ea23542b72c78df833c4679aa51c29c5c3e94f3b935f58fdce955e"
+  url "https://github.com/monroestephenson/opn/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "78ea9c4b37264d69dd7e9eede842756d5b67f4a2fe6bb797b20870b29b62e544"
   license "MIT"
 
   depends_on "rust" => :build
